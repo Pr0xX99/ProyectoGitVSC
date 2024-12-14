777f17d97b7956b2d5d02c8574458613ef8ed07c
Pr0xX99 <saulperezbenet@gmail.com>
2024-12-14 19:55:43 +0100

    "Añadido archivo Tareas.java"

---
+++ Tareas.java
@@ -0,0 +1,14 @@
+public class Tareas {
+    public static void imprimirTareas() {
+        System.out.println("Lista de tareas: 1. Aprender Git, 2. Practicar Java");
+    }
+
+    public static void agregarTarea() {
+        System.out.println("Tarea agregada.");
+    }
+
+    public static void main(String[] args) {
+        imprimirTareas();
+        agregarTarea();
+    }
+}
