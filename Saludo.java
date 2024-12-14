06e43d701f511f96c20a5b34b8152cd55811f873
Pr0xX99 <saulperezbenet@gmail.com>
2024-12-14 20:11:22 +0100

    Creado el archivo Saludo.java

---
+++ Saludo.java
@@ -0,0 +1,14 @@
+public class Saludo {
+    public static void saludoInicial() {
+        System.out.println("¡Hola! Bienvenido al proyecto.");
+    }
+
+    public static void despedida() {
+        System.out.println("¡Adiós! Gracias por participar.");
+    }
+
+    public static void main(String[] args) {
+        saludoInicial();
+        despedida();
+    }
+}
